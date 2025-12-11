9a15d76c29b018ceae952b42d1c318e6c0ea4878
bakir_m <bakir_m@etna-alternance.net>
2025-12-11 14:56:37 +0100

    clear

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -52,7 +52,7 @@ export default function Home() {
             </div>
 
             <a
-              href="/Cvpro.pdf"
+              href="/Cv - pro.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="cv-btn"
